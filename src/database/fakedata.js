e555e6a0c36ea2330375f6ed58a1c6a788a4a785
Elisio Wander <elisio741@hotmail.com>
2020-10-28 23:55:26 -0300

    happy-application

---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng: "49.6455874",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/flagged/photo-1576028246561-d6e04a34adbf?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1567791124560-c60b7d227623?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instrunctions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas Das 18h até 8h",
+        open_on_weekeds: "1"
+    },
+    {
+        id: 2,
+        lat: "-27.222633",
+        lng: "49.6455874",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontram em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1601180964401-6e474136af83?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instrunctions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horários de visitas Das 18h até 8h",
+        open_on_weekeds: "0"
+    }
+]
