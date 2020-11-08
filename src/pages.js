1e039e59217f27194b48367a07e74bfc2b8fb97e
Elisio Wander <elisio741@hotmail.com>
2020-11-08 20:25:53 -0300

    happy-application

--- src/pages.js
+++ src/pages.js
@@ -1,3 +1,5 @@
+const orphanages = require('./database/fakedata')
+
 module.exports = {
     index(req, res) {
         const city = req.query.city
@@ -8,7 +10,7 @@ module.exports = {
         return res.render('orphanage')
     },
     orphanages(req, res) {
-        return res. render('orphanages')
+        return res. render('orphanages', { orphanages })
     },
     createOrphanage(req, res) {
         return res.render('create-orphanage')
